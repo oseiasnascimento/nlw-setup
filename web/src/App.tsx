1f22ec40d1012321dc8419777018015b09e1ff9b
Oseias Saboia <oseiassaboia2@gmail.com>
2023-01-18 16:32:09 -0300

    changing the index.html

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -4,13 +4,13 @@ import { Habit } from "./components/Habit"
 
 function App() {
   return (
-      <>
+      <div className='bg-zinc-500'>
       <Habit completed={3}></Habit>
       <Habit completed={6}></Habit>
       <Habit completed={9}></Habit>
       <Habit completed={5}></Habit>
       <Habit completed={6}></Habit>
-      </>
+      </div>
   )
 }
 
