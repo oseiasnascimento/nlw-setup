39635af111f6be0ebdbd948846b344f3d81f7ed3
Oseias Saboia <oseiassaboia2@gmail.com>
2023-01-19 11:15:27 -0300

    isolating backend files

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,26 +1,14 @@
 //Back-end API RESTfull
 import { fastify } from 'fastify'
 import cors from '@fastify/cors'
-import { PrismaClient } from '@prisma/client'
+import { appRoutes } from './routes'
 
 const app = fastify()
 const hostname = 'localhost'
 const port = 3333 
-const prisma = new PrismaClient()
 
 app.register(cors)
-
-app.get('/habits', async () => {
-
-  const habits = await prisma.habit.findMany({
-    where:{
-      title: {
-        startsWith: 'Beber'
-      }
-    }
-  })
-  return habits
-})
+app.register(appRoutes)
 
 app.listen({
   port: port
