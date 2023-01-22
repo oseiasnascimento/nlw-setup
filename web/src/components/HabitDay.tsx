c83eeded35afe8b7ee045937fc1d9288f99ad146
Oseias Nascimento <oseiassaboia2@gmail.com>
2023-01-22 10:27:57 -0300

    creating progress bar

--- web/src/components/HabitDay.tsx
+++ web/src/components/HabitDay.tsx
@@ -1,9 +1,24 @@
+import * as Popover from '@radix-ui/react-popover';
+import { ProgressBar } from './ProgerssBar';
 interface HabitDayProps {
   completed: number | 0
 }
 
 export function HabitDay() {
-  return (
-    <div className="bg-zinc-900 w-10 h-10 text-white border-2 border-zinc-800 rounded-lg flex items-center justify-center"></div>
-  )
+  return  (
+  <Popover.Root>
+    <Popover.Trigger className="bg-zinc-900 w-10 h-10 text-white border-2 border-zinc-800 rounded-lg" />
+
+    <Popover.Portal>
+      <Popover.Content className='min-w-[320px] p-6 rounded-2xl bg-zinc-900 flex flex-col' >
+        <span className="font-semibold text-zinc-400">domingo</span>
+        <span className="font-semibold mt-1 leading-tight text-3xl">22/01</span>
+
+        <ProgressBar progress={65} />
+
+        <Popover.Arrow height={8} width={16} className='fill-zinc-900' />
+      </Popover.Content>
+    </Popover.Portal>
+  </Popover.Root>
+)
 }
