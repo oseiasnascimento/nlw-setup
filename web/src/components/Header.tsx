583086f4c79014f93691bd9b1857aed326ad6e92
Oseias Saboia <oseiassaboia2@gmail.com>
2023-01-23 16:27:54 -0300

    completion of the form in Front-end

--- web/src/components/Header.tsx
+++ web/src/components/Header.tsx
@@ -12,19 +12,19 @@ export function Header() {
       <Dialog.Root>
         <Dialog.Trigger
           type="button"
-          className="border border-violet-500 font-semibold rounded-lg px-6 py-4 flex items-center gap-3 hover:border-violet-300 hover:bg-violet-500 transition-colors hover:text-white group "
+          className="border border-violet-500 font-semibold rounded-lg px-6 py-4 flex items-center gap-3 hover:border-violet-300 hover:bg-violet-500 hover:text-white transition-colors group "
         >
-          <Plus size={20} className="text-violet-500 group" />
+          <Plus size={20} className="text-violet-500 group-hover:text-white group-transition-colors" />
           Novo hábito
         </Dialog.Trigger>
 
         <Dialog.Portal>
-          <Dialog.Overlay className='w-screen h-screen bg-black/80 fixed inset-0' />
-          <Dialog.Content className='absolute p-10 bg-zinc-900 rounded-2xl w-full max-w-md top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2'>
-            <Dialog.Close className='absolute right-6 top-6 text-zinc-400 hover:text-zinc-200' >
-              <X size={24} aria-label='Fechar' />
+          <Dialog.Overlay className="w-screen h-screen bg-black/80 fixed inset-0" />
+          <Dialog.Content className="absolute p-10 bg-zinc-900 rounded-2xl w-full max-w-md top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2">
+            <Dialog.Close className="absolute right-6 top-6 text-zinc-400 hover:text-zinc-200">
+              <X size={24} aria-label="Fechar" />
             </Dialog.Close>
-            <Dialog.Title className='text-3xl leading-tight font-extrabold' >
+            <Dialog.Title className="text-3xl leading-tight font-extrabold">
               Criar Hábito
             </Dialog.Title>
             <NewHabitForm />
--- web/src/components/NewHabitForm.tsx
+++ web/src/components/NewHabitForm.tsx
@@ -1,8 +1,40 @@
+import * as Checkbox from '@radix-ui/react-checkbox'
 import { Check } from 'phosphor-react'
+import { FormEvent, useState } from 'react'
+
+const availableWeedDays = [
+  'Domingo',
+  'Segunda-feira',
+  'Terça-feira',
+  'Quarta-feira',
+  'Quinta-feira',
+  'Sexta-feira',
+  'Sábado'
+]
 
 export function NewHabitForm() {
+  const [title, setTitle] = useState('')
+  const [weekDays, setweekDays] = useState<number[]>([])
+
+  function createNewHabit(event: FormEvent) {
+    event.preventDefault()
+  }
+
+  function handleToggleWeekDay(weedDay: number) {
+    if(weekDays.includes(weedDay)){
+      const weekDayWithRemoveOne = weekDays.filter(day => day !== weedDay)
+
+      setweekDays(weekDayWithRemoveOne)
+    } else {
+      const weekDayWithAddedOne = [...weekDays, weedDay]
+
+      setweekDays(weekDayWithAddedOne)
+    }
+
+  }
+
   return (
-    <form className="w-full flex flex-col mt-6">
+    <form onSubmit={createNewHabit} className="w-full flex flex-col mt-6">
       <label htmlFor="title" className="font-semibold leading-tight">
         Qual seu comprometimento?
       </label>
@@ -13,11 +45,31 @@ export function NewHabitForm() {
         placeholder="ex.: Exercícios, dormir bem, etc..."
         className="p-4 rounded-lg mt-3 bg-zinc-800 text-white hover:placeholder:text-zinc-400"
         autoFocus
+        onChange={event => setTitle(event.target.value)}
       />
 
       <label htmlFor="" className="font-semibold leading-tight mt-4">
         Qual a recorrência?
       </label>
+
+      <div className="mt-3 flex flex-col gap-2">
+        {availableWeedDays.map((weekDay, i) => {
+          return (
+            <Checkbox.Root
+              key={weekDay}
+              className="flex items-center gap-3 group"
+              onCheckedChange={() => handleToggleWeekDay(i)}
+            >
+              <div className="h-8 w-8 rounded-lg flex items-center justify-center bg-zinc-900 border-2 border-zinc-800 group-data-[state=checked]:bg-green-500 group-data-[state=checked]:border-green-500 ">
+                <Checkbox.Indicator>
+                  <Check size={20} className="text-white" />
+                </Checkbox.Indicator>
+              </div>
+              <span className="text-white leading-tight">{weekDay}</span>
+            </Checkbox.Root>
+          )
+        })}
+      </div>
 
       <button
         type="submit"
