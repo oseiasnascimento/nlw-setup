341f91dfea941c2cf50aea06ef6b3c464762d5c2
Oseias Saboia <oseiassaboia2@gmail.com>
2023-01-24 16:15:15 -0300

    disable past dates

--- web/src/components/HabitsList.tsx
+++ web/src/components/HabitsList.tsx
@@ -1,37 +1,63 @@
-import * as Checkbox from "@radix-ui/react-checkbox";
-import { Check } from "phosphor-react";
-import { useEffect } from "react";
-import { api } from "../lib/axios";
+import * as Checkbox from '@radix-ui/react-checkbox'
+import dayjs from 'dayjs'
+import { Check } from 'phosphor-react'
+import { useEffect, useState } from 'react'
+import { api } from '../lib/axios'
 
-interface HabitsListProps{
+interface HabitsListProps {
   date: Date
 }
 
-export function HabitsList({date}: HabitsListProps) {
+interface HabitsInfo {
+  possibleHabits: {
+    id: string
+    title: string
+    created_at: string
+  }[]
+  completedHabits: string[]
+}
+
+export function HabitsList({ date }: HabitsListProps) {
+  const [habitsInfo, setHabitsInfo] = useState<HabitsInfo>()
 
   useEffect(() => {
-    api.get('day', {
-      params: {
-        date: date.toISOString()
-      }
-    }).then( res => {
-      
-    })
+    api
+      .get('day', {
+        params: {
+          date: date.toISOString()
+        }
+      })
+      .then(res => {
+        setHabitsInfo(res.data)
+      })
   }, [])
+
+  const isDateInPast = dayjs(date)
+  .endOf('day')
+  .isBefore(new Date())
 
   return (
     <>
       <div className="mt-6 flex flex-col gap-3">
-        <Checkbox.Root className="flex items-center gap-3 group">
-          <div className="h-8 w-8 rounded-lg flex items-center justify-center bg-zinc-900 border-2 border-zinc-800 group-data-[state=checked]:bg-green-500 group-data-[state=checked]:border-green-500 ">
-            <Checkbox.Indicator>
-              <Check size={20} className="text-white" />
-            </Checkbox.Indicator>
-          </div>
-          <span className="font-semibold text-xl text-white leading-tight group-data-[state=checked]:text-zinc-400 ">
-            Oseias Saboia
-          </span>
-        </Checkbox.Root>
+        {habitsInfo?.possibleHabits.map(habit => {
+          return (
+            <Checkbox.Root
+              key={habit.id}
+              checked={habitsInfo.completedHabits.includes(habit.id)}
+              disabled={isDateInPast}
+              className="flex items-center gap-3 group"
+            >
+              <div className="h-8 w-8 rounded-lg flex items-center justify-center bg-zinc-900 border-2 border-zinc-800 group-data-[state=checked]:bg-green-500 group-data-[state=checked]:border-green-500 ">
+                <Checkbox.Indicator>
+                  <Check size={20} className="text-white" />
+                </Checkbox.Indicator>
+              </div>
+              <span className="font-semibold text-xl text-white leading-tight group-data-[state=checked]:text-zinc-400 ">
+                {habit.title}
+              </span>
+            </Checkbox.Root>
+          )
+        })}
       </div>
     </>
   )
